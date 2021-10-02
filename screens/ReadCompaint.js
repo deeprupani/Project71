1b19fbd4f2084720f017f443ff0f883c7de4d23a
Deep Rupani <deep.l.rupani@gmail.com>
2021-10-02 12:23:06 +0530

    Finished Reading

--- screens/ReadCompaint.js
+++ screens/ReadCompaint.js
@@ -1,21 +1,51 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import { doc, getDoc } from "firebase/firestore";
+import db from "../config";
 
 export default class ReadComplaint extends React.Component {
-    render(){
-        return(
-            <View style={styles.container}>
-                <Text>Read Story</Text>
-            </View>
-        );
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: "",
+      student: "",
+      query: "",
+    };
+  }
+
+  submitQuery = async () => {
+     const docRef = db.collection("complaints")// .doc("EdHlBIcjoK2TYdafEL9K");
+
+     docRef.get()
+     .then((querySnapshot) => {
+         querySnapshot.forEach((doc) => {
+             // doc.data() is never undefined for query doc snapshots
+             console.log(doc.id, " => ", doc.data());
+         });
+     })
+     .catch((error) => {
+         console.log("Error getting documents: ", error);
+     });``
+
+
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <TouchableOpacity onPress={this.submitQuery}>
+          <Text>Read</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
